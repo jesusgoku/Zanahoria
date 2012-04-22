9d491ce88bfd2cb2623e440f0cd902d556d4103b
jesusgoku <jesus.urrutia@gmail.com>
2012-04-22 04:25:25 -0300

    Cambiarda a la documentacion al archivo .h

--- include/my_function.h
+++ include/my_function.h
@@ -8,15 +8,66 @@
 
 typedef enum {False, True} Bool;
 
+/** Como el print de python da un salto de carro luego de imprimir
+* @param formato cadena de formato
+* @param ... los distintos atributos especificados en formato
+*
+* @return cantidad de parametros imprimidos
+*/
 int println(const char *formato, ...);
+
+/** Redondea un flotante a entero
+*
+* @param num numero que desea ser redondeado
+*
+* @return numero redondeado
+*/
 int redondeoEntero(float num);
+
+/** Limpiar Buffer de Entrada
+*/
 void clearScr();
+
+/** Limpiar Pantalla
+*/
 void clearStdin();
+
+/** Introducir una pausa en la ejecucion
+*/
 void pausa();
+
+/** Muestra un mensaje e introduce una pausa
+*
+* @param mensaje mensaje que se desea mostrar antes de introducir la pausa
+*/
 void pausaMensaje(const char *mensaje);
+
+/** Pregunta al usuario dando la posibilidad de responder y/n
+*
+* @param pregunta pregunta a mostrar al usuario
+*
+* @return respuesta del usuario, 1 para y 0 para n
+*/
 int preguntayn(const char *pregunta);
 
+/** Captura una cadena desde la entrada estadar
+*
+* Mas segura que gets ya que se debe especificar el limite a capturar
+* y se elimina el "ENTER" dejado por fgets
+*
+* @param cadena indica donde se guardara la cadena capturada
+* @param max langitud maxima de caracteres a capturar
+*/
 void mygets(char *cadena, const int max);
+
+/** Pide una cadena al usuario de forma segura
+*
+* Para su utilizacion utiliza la funcion mygets de esta misma libreria
+*
+* @param mensaje indicacion de la cadena a ingresar
+* @param destino cadena donde se guardara la cadena ingresada por el usuario
+* @param max longitud maxima de caracteres a capturar
+*/
 void pedirCadena(const char *mensaje, char *destino, const int max);
 
 #endif // __MY_FUNCTION_H__
